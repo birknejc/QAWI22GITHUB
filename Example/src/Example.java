f5deed8a952197ce636dc9868a919daf0353565a
harrisonBirkner <57458202+harrisonBirkner@users.noreply.github.com>
2022-12-06 19:58:26 -0600

    added another println

--- Example/src/Example.java
+++ Example/src/Example.java
@@ -1,5 +1,7 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("First Github project");
+
+        System.out.println("version two message");
     }
 }
